fb98bcaab9b171e25852065cce1dc64be6e5f321
brandonbuhr <brandonbuhr35@gmail.com>
2025-02-01 16:53:41 -0600

    updated icon height and spacing

--- components/Iconbar.tsx
+++ components/Iconbar.tsx
@@ -5,34 +5,49 @@ function Iconbar() {
   return (
     <>
       <nav>
-        <ul className="flex justify-center space-x-10 pl-10 pr-10">
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl">
-            <Image src="/house-solid.svg" alt="house" width={80} height={80} />
+        <ul className="flex justify-center space-x-10">
+          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl transform scale-100 hover:scale-[1.2] transition duration-200">
+            <Image
+              src="/house-solid.svg"
+              alt="house"
+              width={102}
+              height={100}
+            />
             <li className="mt-5">New Construction</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl">
-            <Image src="/pencil-solid.svg" alt="house" width={70} height={80} />
+          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl transform scale-100 hover:scale-[1.2] transition duration-200">
+            <Image
+              src="/pencil-solid.svg"
+              alt="house"
+              width={90}
+              height={100}
+            />
             <li className="mt-5">Remodeling</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl">
+          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl transform scale-100 hover:scale-[1.2] transition duration-200">
             <Image
               src="/people-roof-solid.svg"
               alt="house"
-              width={85}
-              height={80}
+              width={113}
+              height={100}
             />
             <li className="mt-5">Roofing</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl">
-            <Image src="/tree-solid.svg" alt="house" width={60} height={80} />
+          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl transform scale-100 hover:scale-[1.2] transition duration-200">
+            <Image src="/tree-solid.svg" alt="house" width={79} height={100} />
             <li className="mt-5">Tree Removal</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl">
-            <Image src="/window.png" alt="house" width={70} height={80} />
+          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl transform scale-100 hover:scale-[1.2] transition duration-200">
+            <Image src="/window.png" alt="house" width={90} height={100} />
             <li className="mt-5">Windows & Siding</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl">
-            <Image src="/hammer-solid.svg" alt="house" width={78} height={80} />
+          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-5 rounded-3xl transform scale-100 hover:scale-[1.2] transition duration-200">
+            <Image
+              src="/hammer-solid.svg"
+              alt="house"
+              width={102}
+              height={100}
+            />
             <li className="mt-5">Woodwork</li>
           </div>
         </ul>
