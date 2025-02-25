1e6ac4dc8dedc118e8bafa9d60598c774c148b08
brandonbuhr <brandonbuhr35@gmail.com>
2025-02-24 18:40:39 -0600

    started testimonials page

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -1,10 +1,126 @@
 import Footer from "@/components/Footer";
+import Mainbanner from "@/components/Mainbanner";
 import Navbar from "@/components/Navbar";
 
 function Page() {
   return (
     <div>
       <Navbar />
+      <Mainbanner />
+      <div className="mr-[20%] ml-[20%]">
+        <h1 className="mt-[8%]">Building a Reputation for Satisfaction</h1>
+        <h3 className="mt-[1%]">
+          Our high quality work and friendly approach is proven in the
+          testimonials of our many satisfied customers. Hear from local families
+          and businesses—in their own words—how Buhr Construction has made their
+          building experience better.
+        </h3>
+        <h2 className="mt-[8%]">Good hardworking and friendly people...</h2>
+        <p className="mt-[1%]">
+          “Thank you for the excellent job your men did on my roof. You have
+          real good hardworking and friendly people working for you. Thanks
+          again.”
+          <p className="mt-[1%]">-Dores</p>
+        </p>
+        <h2 className="mt-[8%]">
+          Treated us fairly and honestly throught the project...
+        </h2>
+        <p className="mt-[1%]">
+          “We felt that all your employees did a great job in keeping the good
+          reputation you have established. Tom was always good at answering our
+          questions and keeping his appointments with us. He was friendly, made
+          good suggestions and always returned our phone calls. We also felt
+          lucky that Junior was the foreman for this project. He is a skilled
+          worker who helped us to make decisions that improved the final look of
+          our house. The remainder of the workers, Brian, Scott and others who
+          helped, were also very hard workers. We appreciate their friendliness
+          and good manners. They seemed to enjoy their work while doing it in a
+          professional way. We would also like to thank you. We felt you treated
+          us fairly and honestly throughout the project. Again, thanks for a
+          great job!” -Kevin and Carol
+        </p>
+        <h2 className="mt-[8%]">
+          The thought of having work done on the house no longer feels like an
+          ordeal...
+        </h2>
+        <p className="mt-[1%]">
+          “We want to thank you for a wonderful job. It was a joy working with
+          Buhr Construction. Your men were fantastic. They did a perfect job and
+          left the yard spick and span. In the past weeks I have recommended
+          Buhr Construction to four of my friends who needed roof work. The
+          thought of having work done on the house no longer feels like an
+          ordeal. I look forward to working with you again.”
+        </p>
+        <h2 className="mt-[8%]">Your crew was super and we are thrilled...</h2>
+        <p className="mt-[1%]">
+          “Just wanted to let you know how very pleased we are with your work.
+          Your crew was super and we are thrilled with our new roof. We will
+          most definitely spread the good word.” -Darrell and Lori
+        </p>
+        <h2 className="mt-[8%]">
+          Professional, timely and easy to work with...
+        </h2>
+        <p className="mt-[1%]">
+          “We want to compliment you on your project coordinator Tom, and the
+          crew that worked on our house. Tom was professional, timely and easy
+          to work with. The crew that was sent to our house to install the roof
+          and skylights were so hardworking and did an excellent job. Please
+          pass on to all of them that we are very pleased. We highly recommend
+          Buhr Construction.” -Mary and Dave
+        </p>
+        <h2 className="mt-[8%]">Hats off to you and your man...</h2>
+        <p className="mt-[1%]">
+          “I want to compliment you and your crew for the exceptional job you
+          did replacing the roof on our home. Corey and his crew were the most
+          polite and professional group of young men I have had the pleasure of
+          being around in a long time. I realize having the owners hang around
+          can get a little taxing at times but they always answered my questions
+          and explained things to us. Again, hats off to you and your men for an
+          excellent job on our home.” -Mark and Mary
+        </p>
+        <h2 className="mt-[8%]">I surely will recommend Buhr...</h2>
+        <p className="mt-[1%]">
+          “I’m very satisfied with the roof and skylights you installed at my
+          cottage. I was especially pleased with how clean you left the cottage,
+          both inside and outside. I surely will recommend Buhr Construction if
+          the occasion occurs. Once again, thanks for a job well done.” -Dan
+        </p>
+        <h2 className="mt-[8%]">Nice to work with...</h2>
+        <p className="mt-[1%]">
+          “Your crews were sure nice to work with during the projects at our
+          house. Thanks for a nice job. The new roof looks great!” -Dave
+        </p>
+        <h2 className="mt-[8%]">Quick response to fix tree damage...</h2>
+        <p className="mt-[1%]">
+          “Thank you so much for your quick response fix our tree-damaged
+          garage. The Buhr Construction team did an outstanding and professional
+          job. We will gladly spread the word of your company. Good job and
+          thanks again!” -Jim and Kathy
+        </p>
+        <h2 className="mt-[8%]">Quality workmanship and crews...</h2>
+        <p className="mt-[1%]">
+          “Thank you for the quality workmanship and crews you provided to our
+          jobsite in Sturgeon Bay. It was truly a pleasure to work with Cory,
+          Brian, Dan and Jason over the course of the three-week effort. They
+          are to be commended for their honesty, integrity and quality efforts.
+          I hope to be able to do business with you and them in the future.”
+          -Kenneith A. Rock Rock Construction Company
+        </p>
+        <h2 className="mt-[8%]">Terrific job on our barn roof...</h2>
+        <p className="mt-[1%] mb-[8%]">
+          “The Buhr Construction team did a terrific job on our barn roof. The
+          project was a roof replacement on a 150-year-old barn. The roof was
+          very high and the tear off was a dirty job. They conscientiously
+          looked for and repaired and/or replaced the deteriorated and rotted
+          sections of the old roof before proceeding with the new roof.” “The
+          team shared a strong work ethic and took pride in their work. The
+          quality of their craftsmanship was outstanding, and even the cleanup
+          was well done. Andy and Toby were good communicators during the
+          project to ensure expectations were met or exceeded. I am very please
+          with the new roof. Thanks again to the team for a job well done!””
+          -Roderick
+        </p>
+      </div>
       <Footer />
     </div>
   );
