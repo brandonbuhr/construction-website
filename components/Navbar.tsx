17b1cd0491f70cbdc81477a4d9a8018155c967aa
brandonbuhr <brandonbuhr35@gmail.com>
2025-02-25 18:46:02 -0600

    fixed font and animation on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,35 +4,37 @@ function Navbar() {
   return (
     <>
       <nav className="mt-8">
-        <ul className="flex justify-center space-x-16">
-          <li>
-            <Image
-              src="/logo-buhr-construction.png"
-              alt=""
-              width={180}
-              height={150}
-              className="pt-4"
-            />
-          </li>
-          <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:bg-blue-200 p-2 rounded-3xl transform scale-100 hover:scale-[1.1]">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:bg-blue-200 p-2 rounded-3xl transform scale-100 hover:scale-[1.1]">
-            <Link href="about-us">About Us</Link>
-          </li>
-          <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:bg-blue-200 p-2 rounded-3xl transform scale-100 hover:scale-[1.1]">
-            <Link href="services">Services</Link>
-          </li>
-          <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:bg-blue-200 p-2 rounded-3xl transform scale-100 hover:scale-[1.1]">
-            <Link href="testimonials">Testimonials</Link>
-          </li>
-          <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:bg-blue-200 p-2 rounded-3xl transform scale-100 hover:scale-[1.1]">
-            <Link href="gallery">Gallery</Link>
-          </li>
-          <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:bg-blue-200 p-2 rounded-3xl transform scale-100 hover:scale-[1.1]">
-            <Link href="contact-us">Contact Us</Link>
-          </li>
-        </ul>
+        <div className="ml-[20%] mr-[20%]">
+          <ul className="flex justify-center space-x-8">
+            <li>
+              <Image
+                src="/logo-buhr-construction.png"
+                alt=""
+                width={180}
+                height={150}
+                className="pt-4"
+              />
+            </li>
+            <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:underline">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:underline">
+              <Link href="about-us">About Us</Link>
+            </li>
+            <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:underline">
+              <Link href="services">Services</Link>
+            </li>
+            <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:underline">
+              <Link href="testimonials">Testimonials</Link>
+            </li>
+            <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:underline">
+              <Link href="gallery">Gallery</Link>
+            </li>
+            <li className="mt-9 pt-6 pl-4 pr-4 text-xl hover:cursor-pointer hover:underline">
+              <Link href="contact-us">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
