30302e35cdd9be08164d3e9d5eddc25e2f496e53
brandonbuhr <brandonbuhr35@gmail.com>
2025-02-26 18:30:58 -0600

    added contact form

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,10 +1,23 @@
+import ContactForm from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Mainbanner from "@/components/Mainbanner";
 import Navbar from "@/components/Navbar";
+import React from "react";
 
 function Page() {
   return (
     <div>
       <Navbar />
+      <Mainbanner />
+      <div>
+        <h1>Contact Buhr Construction</h1>
+        <h3>Let's talk about your next project</h3>
+        <p>
+          Request a quote, schedule a consultation, or get more information
+          about our products and services by filling out the form below.
+        </p>
+        <ContactForm />
+      </div>
       <Footer />
     </div>
   );
