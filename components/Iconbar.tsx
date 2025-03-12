4f37c9e60f3ca83d3537d1d90bbc21207c5a6679
brandonbuhr <brandonbuhr35@gmail.com>
2025-03-12 18:24:42 -0500

    fixed icon centering when changing screen size

--- components/Iconbar.tsx
+++ components/Iconbar.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-//width 80px height 80px color #007ec2
 
 function Iconbar() {
   return (
     <>
       <nav>
-        <ul className="flex flex-wrap justify-center space-x-10 sm:flex-wrap md:flex-wrap">
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl transform scale-100 hover:scale-[1.1] transition duration-200">
+        <ul className="justify-items-center grid xl:grid-cols-6 lg:grid-cols-3 md:grid-cols-3 grid-cols-2">
+          <div className="w-[200px] justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl mr-4 ml-4 transform scale-100 hover:scale-[1.1] transition duration-200">
             <Image
               src="/house-solid.png"
               alt="house"
@@ -15,7 +14,7 @@ function Iconbar() {
             />
             <li className="mt-5">New Construction</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl transform scale-100 hover:scale-[1.1] transition duration-200">
+          <div className="w-[200px] justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl mr-4 ml-4 transform scale-100 hover:scale-[1.1] transition duration-200">
             <Image
               src="/pencil-solid.png"
               alt="house"
@@ -24,7 +23,7 @@ function Iconbar() {
             />
             <li className="mt-5">Remodeling</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl transform scale-100 hover:scale-[1.1] transition duration-200">
+          <div className="w-[200px] justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl mr-4 ml-4 transform scale-100 hover:scale-[1.1] transition duration-200">
             <Image
               src="/people-roof-solid.png"
               alt="house"
@@ -33,11 +32,11 @@ function Iconbar() {
             />
             <li className="mt-5">Roofing</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl transform scale-100 hover:scale-[1.1] transition duration-200">
+          <div className="w-[200px] justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl mr-4 ml-4 transform scale-100 hover:scale-[1.1] transition duration-200">
             <Image src="/tree-solid.png" alt="house" width={100} height={100} />
             <li className="mt-5">Tree Removal</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl transform scale-100 hover:scale-[1.1] transition duration-200">
+          <div className="w-[200px] justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl mr-4 ml-4 transform scale-100 hover:scale-[1.1] transition duration-200">
             <Image
               src="/window-icon.png"
               alt="house"
@@ -46,7 +45,7 @@ function Iconbar() {
             />
             <li className="mt-5">Windows & Siding</li>
           </div>
-          <div className="flex-col justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl transform scale-100 hover:scale-[1.1] transition duration-200">
+          <div className="w-[200px] justify-items-center text-center hover:bg-blue-200 hover:cursor-pointer p-6 rounded-3xl mr-4 ml-4 transform scale-100 hover:scale-[1.1] transition duration-200">
             <Image
               src="/hammer-solid.png"
               alt="house"
