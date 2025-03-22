ba3cda6b2116a20272e7583efc610ce92103d67b
brandonbuhr <brandonbuhr35@gmail.com>
2025-03-22 18:52:18 -0500

    all services listed with descriptions

--- components/Services.tsx
+++ components/Services.tsx
@@ -38,6 +38,99 @@ function Services() {
           longevity.
         </p>
       </div>
+      <div className="ml-[20%] mr-[20%]">
+        <h1 className="mt-16 text-2xl text-blue-600">Roofing</h1>
+        <h3 className="text-xl font-bold mt-4">
+          Master Roofing Means Getting it Right the First Time
+        </h3>
+        <p className="mt-4">
+          Because roofing is in our roots, it isn’t just another service
+          offering—it’s our specialty. Our team is led by CertainTeed
+          ShingleMastersTM with a strong commitment to quality. Our crews are
+          trained to use the highest quality materials and installation
+          techniques, adhering to exceptional standards for safety and are all
+          fully insured on the job site. CertainTeed standards also ensure we
+          stand behind our work by offering the longest warranty in the
+          business—a 50-year guarantee on all labor and materials. Our
+          commitment to quality roofing extends beyond skillful labor and
+          materials. Whether your interest is updating to the latest trend with
+          a long-lasting metal roof, or you require a standard tear-off and
+          replacement, our outstanding customer service team will guide you
+          through the process informing you of the latest design trends, quality
+          brands and upgrades within your budget. When it’s time to get started,
+          our crew will show up on time, remain professional and be considerate
+          of your home while employed on your property. Flowerbeds, landscaping
+          and the lawn will be preserved throughout the project and thoroughly
+          cleaned at completion. Browse the Latest Trends We offer the best
+          brand name materials to bring the quality and appearance of your home,
+          business or barn to new heights! Browse CertainTeed’s roofing
+          collection for ideas and inspiration.
+        </p>
+      </div>
+      <div className="ml-[20%] mr-[20%]">
+        <h1 className="mt-16 text-2xl text-blue-600">Tree Removal</h1>
+        <h3 className="text-xl font-bold mt-4">
+          Providing Full-Service Tree Service
+        </h3>
+        <p className="mt-4">
+          At Buhr Construction, we understand that in order to produce quality
+          work we must establish a quality work environment right from the
+          start. Our crews offer more than just tree removal. Hire Buhr
+          Construction for full-service outdoor cleanup including: Brush
+          Trimming and Removal Stump Grinding Lot Clearing Tree Removal As an
+          eco-friendly company, we recycle cleared trees and brush ensuring
+          nothing goes to waste. Our individualized approach to each project
+          allows you to dictate how the wood from your lot is used. With the
+          help of our lumberyard, cleared trees can be turned into lumber for
+          your project, split firewood, or a unique handcrafted creation for
+          your home.
+        </p>
+      </div>
+      <div className="ml-[20%] mr-[20%]">
+        <h1 className="mt-16 text-2xl text-blue-600">Windows & Siding</h1>
+        <h3 className="text-xl font-bold mt-4">
+          Replace, Renew...Enjoy the View!
+        </h3>
+        <p className="mt-4">
+          Using the finest quality brand name materials, our experienced and
+          certified crew will not only bring curb appeal to your home or
+          business, but also the assurance that your new windows and siding will
+          provide optimal insulation for comfort and energy efficiency. Working
+          with Buhr Construction to design the exterior of your home means total
+          customization. Choose from a variety of colors, textures and materials
+          to enhance the appearance of your home and ensure it stands up to even
+          Wisconsin’s harshest weather as well. Additionally, our crew will
+          remain friendly, professional and considerate of your home while
+          employed on your property. Flowerbeds, landscaping and the lawn will
+          be preserved throughout the project and thoroughly cleaned at
+          completion.
+        </p>
+      </div>
+      <div className="ml-[20%] mr-[20%]">
+        <h1 className="mt-16 text-2xl text-blue-600">
+          Custom Woodworking & Lumber
+        </h1>
+        <h3 className="text-xl font-bold mt-4">
+          Build Character with Handcrafted Woodwork
+        </h3>
+        <p className="mt-4 mb-24">
+          Buhr Construction has an on-site sawmill and lumberyard. That means
+          the boards, framing and finishing materials we use for new
+          construction and remodeling projects come from our very own workshop.
+          This allows for total customization! From custom designed crown
+          molding, trim cut to any length, or unique interior woodwork … if you
+          can dream it, we can do it. Also, shop our ever-changing selection of
+          custom outdoor cedar furnishings at our retail store! Choose from
+          pre-stained or unstained options of beautiful handcrafted originals
+          including gazebos, pergolas, arbors, tables, chairs, benches, gliders
+          and more. Visit our lumberyard and retail store at{" "}
+          <b className="text-blue-600">
+            1290 County Road DK, Brussels, WI 54204
+          </b>{" "}
+          or call <b className="text-blue-600">920-825-1318</b> to place an
+          order.
+        </p>
+      </div>
     </>
   );
 }
