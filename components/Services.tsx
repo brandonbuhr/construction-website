324ef0079d033996bb710144916ff9e11ba3af53
brandonbuhr <brandonbuhr35@gmail.com>
2025-03-22 14:16:16 -0500

    first 2 services complete

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,10 +1,12 @@
 function Services() {
   return (
     <>
-      <div className="">
-        <h1>New Construction</h1>
-        <h3>Your Vision - Custom Built - Start to Finish</h3>
-        <p>
+      <div className="ml-[20%] mr-[20%] mt-24">
+        <h1 className="mt-24 text-2xl text-blue-600">New Construction</h1>
+        <h3 className="text-xl font-bold mt-4">
+          Your Vision - Custom Built - Start to Finish
+        </h3>
+        <p className="mt-4">
           If you have a vision for a new home, let us bring it to life! From
           drafting and design to framing and finishing, our team will bring
           full-service building expertise to every stage of the building
@@ -15,6 +17,25 @@ function Services() {
           remain certified and trained in the latest installation techniques,
           and can help you make educated selections of the most durable quality
           materials.
+        </p>
+      </div>
+      <div className="ml-[20%] mr-[20%]">
+        <h1 className="mt-16 text-2xl text-blue-600">Remodeling</h1>
+        <h3 className="text-xl font-bold mt-4">
+          Make Your Home, Barn or Business New Again
+        </h3>
+        <p className="mt-4">
+          Whether your space requires new windows or entirely new walls no job
+          is too big or too small for Buhr Construction. No matter the task, our
+          crew brings the same quality materials, fine craftsmanship and
+          positive approach to the job site. Utilize us in the capacity that
+          works for you and your budget—hire Buhr Construction for the entire
+          job, or as a subcontractor for aspects that require specialized
+          experience. We work closely and respectfully alongside other local
+          area businesses as partners and stewards of your budget. With
+          extensive experience, unsurpassed workmanship, and quality locally
+          purchased materials, Buhr Construction will remodel your space for
+          longevity.
         </p>
       </div>
     </>
