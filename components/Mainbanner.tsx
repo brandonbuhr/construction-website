910cf67d947bb2c191aeac81275e2a7d72e613e4
brandonbuhr <brandonbuhr35@gmail.com>
2025-03-22 19:06:51 -0500

    updated navbar responsiveness for medium screens

--- components/Mainbanner.tsx
+++ components/Mainbanner.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 function Mainbanner() {
   return (
     <>
-      <div className="mt-8 flex justify-center items-center">
+      <div className="mt-16 flex justify-center items-center">
         <div className="absolute text-center text-2xl text-white xl:text-5xl lg:text-5xl md:text-4xl sm:text-3xl">
           <h2 className="font-bold">Quality building for over 20 years.</h2>
         </div>
         <Image
-          className=" border-4 border-solid border-blue-600"
           src="/house1.jpg"
           alt=""
-          width={1000}
+          width={1300}
           height={400}
+          className="rounded-2xl"
         />
       </div>
     </>
