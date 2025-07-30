ee530fc5c6ed5a3af516a1d777dd91475c8c889b
brandonbuhr <brandonbuhr35@gmail.com>
2025-07-30 17:31:17 -0500

    updated contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,57 +2,14 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-import { formSchema } from "@/lib/schemas";
-import { send } from "@/lib/email";
-import { useState, useEffect } from "react";
 import { Textarea } from "./ui/textarea";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+
+const FORMSPREE_FORM_ID = "xblkkoag";
 
 export default function Contact() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      phone: "",
-      email: "",
-      message: "",
-    },
-  });
-
-  const [successMessage, setSuccessMessage] = useState("");
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    try {
-      await send(values);
-
-      setSuccessMessage("Your message was sent successfully!");
-      form.reset();
-    } catch (error) {
-      console.error(error);
-      setSuccessMessage("An error occurred. Please try again.");
-    }
-  }
-
-  useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>;
-
-    if (successMessage) {
-      timer = setTimeout(() => {
-        setSuccessMessage("");
-      }, 4000);
-    }
-
-    return () => clearTimeout(timer);
-  }, [successMessage]);
+  const [state, handleSubmit] = useForm(FORMSPREE_FORM_ID);
 
   return (
     <div className="flex w-full pb-36 pt-8 bg-gradient-to-br from-purple-50 to-purple-100 p-4">
@@ -71,78 +28,77 @@ export default function Contact() {
             </p>
           </div>
         </CardHeader>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-6 p-8"
-          >
-            <FormField
-              control={form.control}
+
+        {state.succeeded ? (
+          <div className="p-8 text-center text-blue-600 font-semibold">
+            Your message was sent successfully!
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit} className="space-y-6 p-8">
+            <Input
+              id="name"
               name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input type="text" placeholder="Name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+              type="text"
+              placeholder="Name"
+              required
+              minLength={2}
+              maxLength={50}
             />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
 
-            <FormField
-              control={form.control}
+            <Input
+              id="phone"
               name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="Phone Number"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+              type="text"
+              placeholder="Phone Number"
+              minLength={2}
+              maxLength={50}
             />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input type="email" placeholder="Email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="message"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Type your message here..."
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+            <ValidationError
+              prefix="Phone"
+              field="phone"
+              errors={state.errors}
             />
 
-            <Button className="bg-blue-600 hover:bg-blue-500" type="submit">
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              required
+              minLength={2}
+              maxLength={50}
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+
+            <Textarea
+              id="message"
+              name="message"
+              placeholder="Type your message here..."
+              required
+              minLength={2}
+              maxLength={500}
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+
+            <Button
+              className="bg-blue-600 hover:bg-blue-500"
+              type="submit"
+              disabled={state.submitting}
+            >
               Submit
             </Button>
-            {successMessage && (
-              <div className="text-center text-blue-600 font-semibold mt-4">
-                {successMessage}
-              </div>
-            )}
           </form>
-        </Form>
+        )}
+
         <CardFooter className="flex flex-col pt-2">
           <p className="text-center text-xs text-muted-foreground">
             We respect your privacy. We'll never share your information.
