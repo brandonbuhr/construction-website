43501094f8b5596ff18b7bf6ce94ec1359dd1aa7
brandonbuhr <brandonbuhr35@gmail.com>
2025-08-02 00:41:52 -0500

    updated career section

--- components/Homesections.tsx
+++ components/Homesections.tsx
@@ -4,26 +4,35 @@ import Link from "next/link";
 function Homesections() {
   return (
     <>
-      <div className="flex flex-col justify-center md:flex-row lg:flex-row sm:items-center items-center bg-blue-200 rounded-2xl pt-8 pb-8">
-        <div className="w-[50%] flex-col">
-          <h1 className="text-center font-bold text-2xl mt-2">
+      <div className="flex flex-col justify-center md:flex-row lg:flex-row sm:items-center items-center rounded-2xl pt-8 pb-8">
+        <div className="w-[50%] flex-col text-center">
+          <h1 className="text-center font-bold text-3xl mt-2">
             Buhr Construction is Hiring!
           </h1>
           <p className="ml-[4%] mr-[4%] mt-12">
-            We're looking to add dedicated, experienced skilled laborers to our
-            team! If you have a strong work ethic and experience in construction
-            trades, we want to hear from you. At Buhr Construction, you'll join
-            a crew known for craftsmanship and professionalism. We take pride in
+            We're looking to add dedicated skilled laborers to our team! If you
+            have a strong work ethic and experience in construction trades, we
+            want to hear from you. At Buhr Construction, you'll join a crew
+            known for craftsmanship and professionalism. We take pride in
             providing ongoing training, top-quality materials, and a safe,
             supportive work environment. Apply today to become a part of our
             growing company!
           </p>
+          <p className="font-bold text-center text-4xl mt-8">(920) 825-1318</p>
+          <p className="font-bold text-center text-4xl mt-8">
+            mbuhr@buhrconstruction.com
+          </p>
+          <Link href="contact-us">
+            <button className="mt-8 mb-8 mr-12 ml-12 bg-blue-600 text-4xl text-white p-4 pl-8 pr-8 rounded-2xl transform scale-100 hover:scale-[1.2] transition duration-200 hover:bg-blue-500">
+              Contact Us
+            </button>
+          </Link>
         </div>
       </div>
       <div className="flex flex-col justify-center md:flex-row lg:flex-row sm:items-center items-center bg-blue-200 rounded-2xl pt-8 pb-8">
         <div className="w-[50%] flex-col">
-          <h1 className="text-center font-bold text-2xl mt-2">
-            Buhr Construction is a ShingleMaster!
+          <h1 className="text-center font-bold text-3xl mt-2">
+            Buhr Construction is a ShingleMaster
           </h1>
           <p className="ml-[4%] mr-[4%] mt-12">
             Our skilled workforce is proudly led by CertainTeed ShingleMasterTM
@@ -43,7 +52,7 @@ function Homesections() {
         </div>
       </div>
       <div className="flex justify-center flex-col mt-12">
-        <h1 className="font-bold text-2xl mt-8 mb-8 text-center">
+        <h1 className="font-bold text-3xl mt-8 mb-8 text-center">
           Hear from our satisfied clients
         </h1>
 
@@ -68,7 +77,7 @@ function Homesections() {
           </Link>
         </div>
         <div className="text-center mt-8">
-          <h1 className="text-2xl font-bold mt-8 mr-12 ml-12">
+          <h1 className="text-3xl font-bold mt-8 mr-12 ml-12">
             Got Questions? Need a Quote?
           </h1>
           <p className="mt-8 mr-12 ml-12">
@@ -85,7 +94,7 @@ function Homesections() {
       </div>
       <div className="mt-32 mr-8 ml-8 flex flex-col items-center">
         <div className="ml-12 mr-12">
-          <h1 className="text-2xl font-bold mt-8">
+          <h1 className="text-3xl font-bold mt-8">
             Browse our collection of work
           </h1>
           <p className="text-xl mt-8 mb-8">
